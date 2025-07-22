c311367da2bda8fc9ca072b42e70ec6e4c50c855
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2025-07-21 20:53:34 -0400

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,28 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/JabcodeJSInterface.js',
-    /*output: {
-        filename: 'jabcodeJSLib.min.js',
-        path: path.resolve(__dirname, 'dist')
-    },*/
-    resolve: {
-        fallback: {
-            path: require.resolve("path-browserify"),
-            fs:false
-        }
-    },
+  mode: 'production',
+  entry: './src/js/JabcodeJSInterface.js',
+  output: {
+    filename: 'jabcodeJSLib.min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve("path-browserify"),
+      fs: false
+    }
+  },
   optimization: {
+    minimize: true,
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          keep_fnames: false,
-          keep_classnames:true
-        }
-      })
-    ]
-  }
+          format: { comments: false },
+          compress: { drop_console: true },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
 };
